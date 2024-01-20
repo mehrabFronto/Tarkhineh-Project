fa2ba72f93092be1ac652a44d7e391910014270c
mehrabFronto <mehrabd20@gmail.com>
2024-01-20 18:54:32 +0330

    fonts added

---
+++ src/constants/localFonts.js
@@ -0,0 +1,55 @@
+import LocalFont from "next/font/local";
+const estedadFont = LocalFont({
+   src: [
+      {
+         path: "../../public/fonts/Estedad/Estedad-Thin.woff2",
+         weight: "100",
+         style: "normal",
+      },
+      {
+         path: "../../public/fonts/Estedad/Estedad-ExtraLight.woff2",
+         weight: "200",
+         style: "normal",
+      },
+      {
+         path: "../../public/fonts/Estedad/Estedad-Light.woff2",
+         weight: "300",
+         style: "normal",
+      },
+      {
+         path: "../../public/fonts/Estedad/Estedad-Regular.woff2",
+         weight: "400",
+         style: "normal",
+      },
+      {
+         path: "../../public/fonts/Estedad/Estedad-Medium.woff2",
+         weight: "500",
+         style: "normal",
+      },
+      {
+         path: "../../public/fonts/Estedad/Estedad-SemiBold.woff2",
+         weight: "600",
+         style: "normal",
+      },
+      {
+         path: "../../public/fonts/Estedad/Estedad-Bold.woff2",
+         weight: "700",
+         style: "normal",
+      },
+      {
+         path: "../../public/fonts/Estedad/Estedad-ExtraBold.woff2",
+         weight: "800",
+         style: "normal",
+      },
+      {
+         path: "../../public/fonts/Estedad/Estedad-Black.woff2",
+         weight: "900",
+         style: "normal",
+      },
+   ],
+   variable: "--font-estedad",
+   style: "normal",
+   display: "block",
+});
+
+export default estedadFont;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+   content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      extend: {
+         fontFamily: {
+            sans: ["var(--font-estedad)", ...fontFamily.sans],
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 };
