09537b1ef475efd077fe2623fb987edb7c0a12d2
mehrabFronto <mehrabd20@gmail.com>
2024-01-24 15:32:51 +0330

    input styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,22 @@
 const Home = () => {
    return (
-      <div>
+      <div className="p-10">
          <h1 className="text-primary-400">خانه</h1>
          <p>متن تستی</p>
+         <div className="p-10">
+            <div className="input__container">
+               <input
+                  id="test"
+                  type="text"
+                  className="input input--md"
+               />
+               <label
+                  htmlFor="test"
+                  className="input__label">
+                  رمز عبور
+               </label>
+            </div>
+         </div>
       </div>
    );
 };
