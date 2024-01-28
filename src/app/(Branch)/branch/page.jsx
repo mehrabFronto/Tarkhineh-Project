7bfadc9ea53e36e7a16f35ac7b7666950329fa6b
mehrabFronto <mehrabd20@gmail.com>
2024-01-28 12:13:26 +0330

    folders organization

---
+++ src/app/(Branch)/branch/page.jsx
@@ -0,0 +1,5 @@
+const BranchPage = () => {
+   return <div>BranchPage</div>;
+};
+
+export default BranchPage;
---
+++ src/app/(Deputize)/deputize/page.jsx
@@ -0,0 +1,5 @@
+const DeputizePage = () => {
+   return <div>DeputizePage</div>;
+};
+
+export default DeputizePage;
